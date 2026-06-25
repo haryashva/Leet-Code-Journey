9ea11123924d53610a0b7a8c894a06894bb2ac33
Harry <haryashvagupta@gmail.com>
2026-06-26 05:28:20 +0530

    Time: 0 ms (100%), Space: 14.9 MB (37.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,52 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-            int sum = nums[i];
-            for (int j = i + 1; j < nums.size(); j++){
-                sum = sum + nums[j];
-                if(sum == target){
-                    return {i,j};
-                }
-                sum = sum - nums[j];
-            }
-        }
-        return {};
+        //   Brute approach
+        //  for (int i = 0; i < nums.size(); i++) {
+        //         for (int j = i + 1; j < nums.size(); j++){
+        //             if(nums[i] + nums[j] == target){
+        //                 return {i,j};
+        //             }
+        //         }
+        //     }
+        //     return {};
+        // }
+
+        //better approach or best for this case.
+        // unordered_map<int,int> mp;
+        // for(int i = 0; i < nums.size(); i++){
+        //     int complement = target - nums[i];
+        //     if(mp.find(complement) /* return key index */ != mp.end()){
+        //         return {mp[complement], i};
+        //     }
+        //     mp[nums[i]] = i;
+        // }
+        // return {};
+       
+       vector<pair<int,int>> numswithindex ;
+       for(int i = 0; i < nums.size() ; i++){
+          numswithindex.push_back({nums[i], i});
+       }
+       
+       sort(numswithindex.begin(),numswithindex.end());
+
+       int left = 0;
+       int right = nums.size() - 1;
+
+       while(left < right){
+           int sum = numswithindex[left].first + numswithindex[right].first;
+           if(sum > target){
+             right--;
+           }
+           if(sum < target){
+            left++;
+           }
+           if(sum == target){
+              return {numswithindex[left].second, numswithindex[right].second};
+           }
+       }
+       return {};
+
+
     }
-};
+    };
